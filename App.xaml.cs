dd8eb16ea0269fcc155bec5e37f66337dd811e0d
kesa <kesa0v0@gmail.com>
2025-03-20 16:22:51 +0900

    init wpf

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace TabletLink_WindowsApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
